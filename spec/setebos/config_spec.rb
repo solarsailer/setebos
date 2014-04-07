cc8dadf05f20be963cd00933936ee720a747fef3
Matthieu Oger <moger@pixelnest.io>
2014-04-07 18:48:42 +0200

    Spec: changes.

--- spec/setebos/config_spec.rb
+++ spec/setebos/config_spec.rb
@@ -7,13 +7,20 @@ describe Setebos::Config do
     @file_02 = Setebos::Config.parse('samples/config02.yml')
   end
 
-  describe "when asked to parse a config file" do
-    it "must retrieve a server" do
-      @file_01[:server][:host].must_equal "setebos"
+  describe 'when asked to parse a config file' do
+    it 'must retrieve a server' do
+      @file_01[:server][:host].must_equal 'setebos'
     end
 
-    it "must retrieve a list of files and a server" do
-      @file_02[:server][:host].must_equal "setebos"
+    it 'must retrieve a list of files and a server' do
+      @file_02[:server][:host].must_equal 'setebos'
+      @file_02[:files].length.must_be :==, 2
+
+      @file_02[:files][0][:from].must_equal 'samples/test01.txt'
+      @file_02[:files][0][:to].must_equal '/tmp'
+
+      @file_02[:files][1][:from].must_equal 'samples/test_with_$_chars_symb_42.md'
+      @file_02[:files][1][:to].must_equal '/tmp/02/'
     end
   end
 
