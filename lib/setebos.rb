e47954118a58d2d15c18e674641b196551ee244a
Matthieu Oger <moger@pixelnest.io>
2014-04-07 16:57:45 +0200

    Setebos: put module into the class name.

--- lib/setebos.rb
+++ lib/setebos.rb
@@ -1,10 +1,7 @@
 require 'setebos/version'
 
-# Setebos module.
-module Setebos
-  class Run
-    def initialize(path)
-      p path
-    end
+class Setebos::Run
+  def initialize(path)
+
   end
 end
