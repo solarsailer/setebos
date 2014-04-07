8b48b4cd74fe00f43c1e0498ed8d8ce2e3e44c9f
Matthieu Oger <moger@pixelnest.io>
2014-04-07 18:49:35 +0200

    Logger: cosmetics + exit on error.

--- lib/setebos/logger.rb
+++ lib/setebos/logger.rb
@@ -2,21 +2,52 @@ require 'colorize'
 
 # Logger.
 class Logger
+
+  # -------------------------------------------------------
+  # Pre-formatted log.
+  # -------------------------------------------------------
+
+  # Public: Print a success message and exit.
+  #
+  # message  - message.
+  #
+  # Returns nothing.
   def self.success(message)
     Logger.log message.colorize(:green).bold
   end
 
-  def self.error(message)
+  # Public: Print an error message and exit.
+  #
+  # message  - message.
+  # exit_now - should exit the program? Default to true.
+  #
+  # Returns nothing.
+  def self.error(message, exit_now: true)
     Logger.log message.colorize(:red).bold
+    exit 1 if exit_now
   end
 
+  # Public: Print an info message and exit.
+  #
+  # message  - message.
+  #
+  # Returns nothing.
   def self.info(message)
     Logger.log message.colorize(:blue)
   end
 
+  # Public: Print a message and exit.
+  #
+  # message  - message.
+  #
+  # Returns nothing.
   def self.message(message)
     Logger.log message
   end
+
+  # -------------------------------------------------------
+  # Raw log.
+  # -------------------------------------------------------
 
   def self.log(message)
     puts message
