d17e1b2c98351e9d65b6772754e37093a3e083df
Matthieu Oger <moger@pixelnest.io>
2014-04-07 18:12:51 +0200

    Spec: changes.

--- spec/setebos/config_spec.rb
+++ spec/setebos/config_spec.rb
@@ -2,10 +2,18 @@ require 'spec_helper'
 
 describe Setebos::Config do
 
+  before do
+    @file_01 = Setebos::Config.parse('samples/config01.yml')
+    @file_02 = Setebos::Config.parse('samples/config02.yml')
+  end
+
   describe "when asked to parse a config file" do
-    it "must load a simple config file" do
-      result = Setebos::Config.parse('samples/config01.yml')
-      puts result
+    it "must retrieve a server" do
+      @file_01[:server][:host].must_equal "setebos"
+    end
+
+    it "must retrieve a list of files and a server" do
+      @file_02[:server][:host].must_equal "setebos"
     end
   end
 
