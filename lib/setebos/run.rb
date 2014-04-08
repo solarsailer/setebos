62a16485bf874b67daa9ff4ce1ab8cf42723fda2
Matthieu Oger <moger@pixelnest.io>
2014-04-08 14:54:53 +0200

    Setebos::Run: test connection + log.

--- lib/setebos/run.rb
+++ lib/setebos/run.rb
@@ -25,10 +25,15 @@ class Setebos::Run
   #
   # Returns a Setebos::Server.
   def create_server(server_section)
+    Logger.info "Create a server for #{server_section.to_s}..."
+
     server = Setebos::Server.new(
       server_section[:host],
       user:     server_section[:user],
       password: server_section[:password]
     )
+
+    reachable = server.test()
+    Logger.error 'The server is not reachable. Abort.' if not reachable
   end
 end
