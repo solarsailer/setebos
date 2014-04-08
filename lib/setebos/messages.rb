4a5da95589c0a2229d8a8953cf55736f3e4d14e9
Matthieu Oger <moger@pixelnest.io>
2014-04-08 18:07:08 +0200

    Server::Run: add `#validate_server_section`.

--- lib/setebos/messages.rb
+++ lib/setebos/messages.rb
@@ -6,6 +6,9 @@ class Setebos::Messages
   CONFIG_INVALID     = 'Invalid file.'
 
   # Setebos::Run.
+  SERVER_NIL       = 'There is no `server` section in the config.'
+  SERVER_NO_HOST   = 'There is no `host` in the `server` section of the config.'
+
   FILES_NIL        = 'There is no `files` section in the config.'
   FILES_NOT_ARRAY  = 'The `files` section is not a list.'
 
@@ -13,7 +16,6 @@ class Setebos::Messages
   SERVER_OK        = 'Server done.'
 
   NOT_REACHABLE    = 'The server is not reachable.'
-
 
   SEND_FILES_START = 'Send files...'
   SEND_FILES_OK    = 'Sending files done.'
--- lib/setebos/run.rb
+++ lib/setebos/run.rb
@@ -22,6 +22,25 @@ class Setebos::Run
 
   private
 
+  # Private: Validate the `server` of the config.
+  #
+  # server_section - `server` section of the config file.
+  #
+  # Returns nothing.
+  # Exit if nil.
+  # Exit if no host.
+  def validate_server_section(server_section)
+    Logger.error Setebos::Messages::SERVER_NIL if not server_section
+    Logger.error Setebos::Messages::SERVER_NO_HOST if not server_section[:host]
+  end
+
+  # Private: Validate the `files` of the config.
+  #
+  # files_section - `files` section of the config file.
+  #
+  # Returns nothing.
+  # Exit if nil.
+  # Exit if not Array.
   def validate_files_section(files_section)
     Logger.error Setebos::Messages::FILES_NIL if not files_section
     Logger.error Setebos::Messages::FILES_NOT_ARRAY if not files_section.kind_of?(Array)
@@ -33,9 +52,10 @@ class Setebos::Run
   #
   # Returns a Setebos::Server.
   def create_server(server_section)
+    validate_server_section(server_section)
     Logger.info Setebos::Messages::SERVER_START % server_section.to_s
 
-    # Create.
+    # Create the server with the server_section of the config file.
     server = Setebos::Server.new(
       server_section[:host],
       user:     server_section[:user],
@@ -59,9 +79,9 @@ class Setebos::Run
   # Returns nothing.
   def send_files(files_section)
     validate_files_section(files_section)
-
     Logger.info Setebos::Messages::SEND_FILES_START
 
+    # Send the file to the server and display error.
     errors = @server.send_files(files_section)
     errors.each do |err|
       Logger.warning Setebos::Messages::FILE_NOT_SEND % err
